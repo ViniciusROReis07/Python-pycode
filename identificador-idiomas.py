2d22e3185a991f6dea6f19e6976c5fdab3f4cc5f
ViniciusROReis07 <guaguinhireis@gmail.com>
2022-11-20 00:27:35 -0300

    Idenficacao de idioma com py

---
+++ identificador-idiomas.py
@@ -0,0 +1,6 @@
+from langdetect import detect
+
+print(detect("Bayern München, gemeinhin als FC Bayern München, FC Bayern, einfach Bayern oder abgekürzt als FCB bezeichnet, ist ein deutscher Multisportverein mit Sitz in der Stadt München im Bundesland Bayern. Das Land und die Stadt, die auf Deutsch Bayern bzw. München heißen, benennen das Team."))
+
+
+
